95c1067ca44d01be83c91b5200c60779d60b7252
moathssnck <myneww00@gmail.com>
2025-07-01 16:15:40 +0300

    WWW

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getDatabase } from "firebase/database";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCEK4irWtP1f6y_J1Gj3kNwsyLdItndt5Y",
-    authDomain: "sdkk-22bbc.firebaseapp.com",
-    projectId: "sdkk-22bbc",
-    storageBucket: "sdkk-22bbc.firebasestorage.app",
-    messagingSenderId: "1039314059862",
-    appId: "1:1039314059862:web:e14e764f77ce86c817b1f7",
-    measurementId: "G-K0L3MMPDR7"
+    apiKey: "AIzaSyDJOaDkXvm5XP3-Fu0LsuKvQ0_dZK5uf-k",
+  authDomain: "school-77936.firebaseapp.com",
+  projectId: "school-77936",
+  storageBucket: "school-77936.firebasestorage.app",
+  messagingSenderId: "783345000886",
+  appId: "1:783345000886:web:585c7579926bcb8f0ca117",
+  measurementId: "G-XX9L099J8H"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
